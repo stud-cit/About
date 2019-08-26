ad3bd32f126b3a8ec5c4f5769a0b9751ae6b16c5
Helsis <seagull.bogdan@gmail.com>
2019-08-27 00:03:23 +0300

    chore( index page ): coercing  a page to layout design

--- client/pages/index.vue
+++ client/pages/index.vue
@@ -2,12 +2,23 @@
   <v-row class="carousel">
     <v-col cols="8" offset="1" v-for="(page, i) in pages" :key="i">
       <v-card :href="page.to" :link="true">
-        <v-img :src="page.img" :lazy-src="page.lazyImg" height="60vh">
+        <v-img
+          :src="page.img"
+          :gradient="imagePageGradient"
+          :lazy-src="page.lazyImg"
+          height="60vh"
+        >
           <v-card-title class="title white--text fill-height">
-            <v-row class="fill-height" justify="center">
-              <span class="display-3 align-self-center" v-text="page.title"/>
+            <v-row justify="center" align="center" class="fill-height" >
+              <span class="headline font-weight-bold font-italic px-4">
+                0{{ ++i }}
+              </span>
+              <span class="display-3 font-weight-bold">
+                {{ page.title }}
+              </span>
             </v-row>
           </v-card-title>
+          <div class="fill-height bottom-gradient"></div>
         </v-img>
       </v-card>
     </v-col>
@@ -26,6 +37,9 @@ import { Getter } from 'vuex-class'
 })
 export default class HomePage extends Vue {
   @Getter('getPageStage') pages;
+
+  imagePageGradient: string =
+    "to top right, rgba(115, 115, 115, .33), rgba(32, 32, 72, .7)";
 }
 </script>
 
