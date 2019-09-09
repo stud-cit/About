454d1c6849e1833b1040f72acb505ac4ce4e1fe9
Electoratium <electoratium@gmail.com>
2019-09-09 22:00:20 +0300

    chore: remove merge conflict

--- client/layouts/immediate.vue
+++ client/layouts/immediate.vue
@@ -1,15 +1,8 @@
 <template>
-<<<<<<< HEAD
-  <v-app > 
+  <v-app> 
     <v-app-bar app dark flat color="transparent">
       <v-toolbar-title>Logotype</v-toolbar-title>
       <v-spacer/>
-=======
-  <v-app>
-    <v-app-bar app flat color="transparent">
-      <v-toolbar-title class="white px-2">Logotype</v-toolbar-title>
-      <v-spacer />
->>>>>>> 0b82bf0b18b537c9e021ad7aa331a98ff865deec
       <v-toolbar-items>
         <v-btn
           v-for="(page, i) in pages"
@@ -29,15 +22,9 @@
 
     <v-img :src="this.getDynamicAssets(cover)" class="imageCover" />
 
-<<<<<<< HEAD
-    <v-content class="pt-0 ">
+    <v-content class="pt-0">
       <v-container  fluid class="fill-height pa-0">
         <nuxt/>
-=======
-    <v-content>
-      <v-container fluid class="fill-height pa-0">
-        <nuxt />
->>>>>>> 0b82bf0b18b537c9e021ad7aa331a98ff865deec
       </v-container>
     </v-content>
   </v-app>
@@ -55,16 +42,9 @@ export default class ImmediatetLayout extends Vue {
 </script>
 
 <style lang="sass">
-<<<<<<< HEAD
   .imageCover
     position: fixed !important
     height: 100vh
     width: 100vw
 
-=======
-.imageCover
-  position: fixed !important
-  height: 100vh
-  width: 100vw
->>>>>>> 0b82bf0b18b537c9e021ad7aa331a98ff865deec
 </style>
--- client/pages/offers.vue
+++ client/pages/offers.vue
@@ -1,5 +1,4 @@
 <template>
-<<<<<<< HEAD
   <v-container class="wrapper" >  
     <v-column v-for="offer in $store.state.weOffers"
         :key="offer">
@@ -36,31 +35,6 @@
       </footer>
     </v-column>
   </v-container>
-=======
-  <v-card class="mx-auto" max-width="400">
-    <v-img
-      class="white--text"
-      height="200px"
-      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
-    >
-      <v-card-title class="align-end fill-height">
-        {{ $route.name }}
-      </v-card-title>
-    </v-img>
-
-    <v-card-text>
-      <span>Number 10</span>
-      <span class="text--primary">
-        <span>{{ $t('welcome') }}</span>
-      </span>
-    </v-card-text>
-
-    <v-card-actions>
-      <v-btn text color="orange">Share</v-btn>
-      <v-btn text color="orange">Explore</v-btn>
-    </v-card-actions>
-  </v-card>
->>>>>>> 0b82bf0b18b537c9e021ad7aa331a98ff865deec
 </template>
 
 <script lang="ts">
