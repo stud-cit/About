3a9acf5ab800aa13d730493e639446c84e6deb50
v-bondarenko364 <v.bondarenko.job@gmail.com>
2019-09-18 21:21:11 +0300

    fix( error page ): make correct work sass

--- client/layouts/error.vue
+++ client/layouts/error.vue
@@ -26,44 +26,44 @@ export default {
 </script>
 
 <style lang="sass">
-	.errors
-		text-align: center
-		width: 100%
+.errors
+	text-align: center
+	width: 100%
+	position: absolute
+	top: 50%
+	transform: translateY(-50%)
+	~ .number
+		font-size: 250px
+		color: rgba(206, 206, 206, 1)
+	~ .description
+		font-size: 150px
+		display: inline
+		color: rgba(218, 218, 218, 1)
+	~ img
+		width: auto
+		height: 250px
+
+.link
+	~ .back
+		background: rgba(242, 242, 242, 1)
+		height: 100vh
+		width: 100px
 		position: absolute
-		top: 50%
-		transform: translateY(-50%)
-		~	.number
-				font-size: 250px
-				color: rgba(206, 206, 206, 1)
-			.description
-				font-size: 150px
-				display: inline
-				color: rgba(218, 218, 218, 1)
-			img
-				width: auto
-				height: 250px
+		right: 0
+		top: 0
+		bottom: 0
+	.text
+		position: absolute
+		transform: rotate(-90deg)
+		font-size: 60px
+		right: 260px
+		top: 0
+		bottom: 0
+		display: flex
+		flex-direction: row-reverse
+		align-items: flex-end
+	:link, :visited, :active, :hover
+		text-decoration: none
+		color: rgba(227, 227, 227, 1)
 
-	.link
-		~	.back
-				background: rgba(242, 242, 242, 1)
-				height: 100vh
-				width: 100px
-				position: absolute
-				right: 0
-				top: 0
-				bottom: 0
-			.text
-				position: absolute
-				transform: rotate(-90deg)
-				font-size: 60px
-				right: 260px
-				top: 0
-				bottom: 0
-				display: flex
-				flex-direction: row-reverse
-				align-items: flex-end
-			:link, :visited, :active, :hover
-				text-decoration: none
-				color: rgba(227, 227, 227, 1)
-
-</style
+</style>
