c3fa91d7ffba3314b5075c92f96317766468fe10
v-bondarenko364 <v.bondarenko.job@gmail.com>
2019-10-15 21:52:19 +0300

    style( about ): add full width use contacts on md

--- client/pages/about.vue
+++ client/pages/about.vue
@@ -1,99 +1,88 @@
 <template>
-	<v-container class="py-0">
-		<v-row justify="center">
-			<v-col cols="12" sm="10">
-				<v-window v-model="curStage" continuous dark>
-					<v-window-item>
-						<PreviewPage
-							:title="$t(about[$i18n.locale].previewTitle)"
-							:subtitle="about[$i18n.locale].previewSubtitle"
-							:description="about[$i18n.locale].previewDescription"
-							:icon-down="false"
-						/>
-					</v-window-item>
-					<v-window-item
-						v-for="(slide, index) in about[$i18n.locale].slides"
-						:key="index"
-					>
-						<v-row justify="start" align="center" class="slide-container">
-							<v-col cols="12" xl="9" lg="12" md="12" sm="12">
-								<v-row>
-									<v-col col="12">
-										<div
-											class="font-weight-bold ma-2 white--text text-uppercase"
-											:style="getSlideTitleFont"
-										>
-											{{ $t('common.companyName') }}
-										</div>
-										<div
-											class="px-xl-12 px-lg-8 px-md-8 px-sm-8 px-xs-8 py-xl-12 py-lg-2 py-md-8 py-sm-8 px-4 py-2 slide-content font-weight-light"
-											:style="getSlideContentFont"
-										>
-											<p class="py-md-12">{{ slide }}</p>
-										</div>
-									</v-col>
-								</v-row>
-							</v-col>
-						</v-row>
-					</v-window-item>
-					<v-window-item class="slide-container"></v-window-item>
-				</v-window>
-			</v-col>
-		</v-row>
-		<p
-			class="d-none d-md-block rotated-phraze font-weight-light"
-			:class="isLgAndUp ? 'rotated-phraze-lg' : 'rotated-phraze-md'"
-			v-if="curStage <= about[$i18n.locale].slides.length"
-		>
-			{{ $t('common.slogan') }}
-		</p>
-		<p
-			class="d-none d-md-block font-weight-light rotated-phraze pointer"
-			:class="isLgAndUp ? 'rotated-phraze-lg' : 'rotated-phraze-md'"
-			v-if="curStage > about[$i18n.locale].slides.length"
-			@click="backToStart"
-		>
-			{{ $t('about.backToStart') }}
-		</p>
+  <v-container class="py-0">
+    <v-row justify="center">
+      <v-col cols="12" sm="10">
+        <v-window v-model="curStage" continuous dark>
+          <v-window-item>
+            <PreviewPage
+              :title="$t(about[$i18n.locale].previewTitle)"
+              :subtitle="about[$i18n.locale].previewSubtitle"
+              :description="about[$i18n.locale].previewDescription"
+              :icon-down="false"
+            />
+          </v-window-item>
+          <v-window-item v-for="(slide, index) in about[$i18n.locale].slides" :key="index">
+            <v-row justify="start" align="center" class="slide-container">
+              <v-col cols="12" xl="9" lg="12" md="12" sm="12">
+                <v-row>
+                  <v-col col="12">
+                    <div
+                      class="font-weight-bold ma-2 white--text text-uppercase"
+                      :style="getSlideTitleFont"
+                    >{{ $t('common.companyName') }}</div>
+                    <div
+                      class="px-xl-12 px-lg-8 px-md-8 px-sm-8 px-xs-8 py-xl-12 py-lg-2 py-md-8 py-sm-8 px-4 py-2 slide-content font-weight-light"
+                      :style="getSlideContentFont"
+                    >
+                      <p class="py-md-12">{{ slide }}</p>
+                    </div>
+                  </v-col>
+                </v-row>
+              </v-col>
+            </v-row>
+          </v-window-item>
+          <v-window-item class="slide-container"></v-window-item>
+        </v-window>
+      </v-col>
+    </v-row>
+    <p
+      class="d-none d-md-block rotated-phraze font-weight-light"
+      :class="isLgAndUp ? 'rotated-phraze-lg' : 'rotated-phraze-md'"
+      v-if="curStage <= about[$i18n.locale].slides.length"
+    >{{ $t('common.slogan') }}</p>
+    <p
+      class="font-weight-light rotated-phraze pointer"
+      :class="isLgAndUp ? 'rotated-phraze-lg' : 'rotated-phraze-md'"
+      v-if="curStage > about[$i18n.locale].slides.length"
+      @click="backToStart"
+    >{{ $t('about.backToStart') }}</p>
 
-		<v-footer absolute color="transparent" class="pb-0 px-0 px-sm-auto">
-			<v-row justify="center">
-				<v-col sm="12" md="12" class="pb-0">
-					<v-row justify-sm="center" justify-md="space-around" align="center">
-						<v-col sm="10" md="8" class="px-0 d-none d-sm-flex">
-							<v-slider
-								v-model="curStage"
-								:max="about[$i18n.locale].slides.length + 1"
-								class="slider"
-								step="1"
-								ticks="always"
-								tick-size="10"
-								hide-details
-								dark
-							/>
-						</v-col>
-						<v-col xs="12" md="auto" class="pb-0" :class="isSmAndDown ? 'justify-center' : ''">
-							<v-card
-								class="pa-4 pt-0 use-contacts-container"
-								@click="() => changeContactBar(true)"
-							>
-								<v-card-title
-									class="justify-center"
-									:style="getUseContactsTitleFont"
-									>{{ $t('contact.title') }}</v-card-title
-								>
-								<v-card-actions
-									class="pa-0 contacts-action justify-center"
-									:style="getUseContactsActionFont"
-									>{{ $t('contact.subTitle') }}</v-card-actions
-								>
-							</v-card>
-						</v-col>
-					</v-row>
-				</v-col>
-			</v-row>
-		</v-footer>
-	</v-container>
+    <v-footer absolute color="transparent" class="pb-0 px-0 px-sm-auto">
+      <v-row justify="center">
+        <v-col sm="12" md="12" class="pb-0">
+          <v-row justify-sm="center" justify-md="space-around" align="center">
+            <v-col sm="10" md="8" class="px-0 d-none d-sm-flex">
+              <v-slider
+                v-model="curStage"
+                :max="about[$i18n.locale].slides.length + 1"
+                class="slider"
+                step="1"
+                ticks="always"
+                tick-size="10"
+                hide-details
+                dark
+              />
+            </v-col>
+            <v-col xs="12" md="12" lg="auto" class="pb-0" :class="{'justify-center' : isSmAndDown}">
+              <v-card
+                class="pa-4 pt-0 use-contacts-container"
+                @click="() => changeContactBar(true)"
+              >
+                <v-card-title
+                  class="justify-center"
+                  :style="getUseContactsTitleFont"
+                >{{ $t('contact.title') }}</v-card-title>
+                <v-card-actions
+                  class="pa-0 contacts-action justify-center"
+                  :style="getUseContactsActionFont"
+                >{{ $t('contact.subTitle') }}</v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-footer>
+  </v-container>
 </template>
 
 <script lang="ts">
@@ -103,108 +92,108 @@ import { Getter, Mutation } from 'vuex-class';
 import PreviewPage from '@/components/preview-page.vue';
 
 @Component({
-	layout: 'immediate',
-	head: {
-		title: 'About us',
-	},
-	components: {
-		PreviewPage,
-	},
+  layout: 'immediate',
+  head: {
+    title: 'About us',
+  },
+  components: {
+    PreviewPage,
+  },
 })
 export default class AboutPage extends Vue {
-	@Getter('getAboutStage') about;
-	@Getter('getPageId') pageId;
-	@Getter('getPageRouteById') getPageRouteById;
-	@Getter('getContactBarVisibility') isShowContactBar;
-	@Mutation('changePageId') changePageId;
-	@Mutation('changeContactBar') changeContactBar;
-	curStage: number = 0;
+  @Getter('getAboutStage') about;
+  @Getter('getPageId') pageId;
+  @Getter('getPageRouteById') getPageRouteById;
+  @Getter('getContactBarVisibility') isShowContactBar;
+  @Mutation('changePageId') changePageId;
+  @Mutation('changeContactBar') changeContactBar;
+  curStage: number = 0;
 
-	handleNavigatingPage(toRight: boolean) {
-		const newPageIndex = toRight ? this.pageId + 1 : this.pageId - 1;
-		const nextPage = this.getPageRouteById(newPageIndex);
-		this.$router.push(nextPage);
-	}
-	backToStart() {
-		this.curStage = 0;
-		this.changeContactBar(false);
-	}
+  handleNavigatingPage(toRight: boolean) {
+    const newPageIndex = toRight ? this.pageId + 1 : this.pageId - 1;
+    const nextPage = this.getPageRouteById(newPageIndex);
+    this.$router.push(nextPage);
+  }
+  backToStart() {
+    this.curStage = 0;
+    this.changeContactBar(false);
+  }
 
-	get isXsOnly() {
-		return this.$breakpoint ? this.$breakpoint.is.xsOnly : false;
-	}
-	get isSmAndDown() {
-		return this.$breakpoint ? this.$breakpoint.is.smAndDown : false;
-	}
-	get isLgAndUp() {
-		return this.$breakpoint ? this.$breakpoint.is.lgAndUp : false;
-	}
+  get isXsOnly() {
+    return this.$breakpoint ? this.$breakpoint.is.xsOnly : false;
+  }
+  get isSmAndDown() {
+    return this.$breakpoint ? this.$breakpoint.is.smAndDown : false;
+  }
+  get isLgAndUp() {
+    return this.$breakpoint ? this.$breakpoint.is.lgAndUp : false;
+  }
 
-	get getSlideTitleFont() {
-		return {
-			fontSize: `${this.getCustomAdaptiveSize({
-				xs: 12,
-				sm: 20,
-				md: 20,
-				lg: 30,
-			})}px`,
-		};
-	}
-	get getSlideContentFont() {
-		return {
-			fontSize: `${this.getCustomAdaptiveSize({
-				xs: 15,
-				sm: 20,
-				md: 20,
-				lg: 30,
-			})}px`,
-		};
-	}
-	get getPreviewTitleFont() {
-		return { fontSize: `${this.getCommonAdaptiveFontSize('previewTitle')}px` };
-	}
-	get getPreviewSubTitleFont() {
-		return {
-			fontSize: `${this.getCommonAdaptiveFontSize('previewSubtitle')}px`,
-		};
-	}
-	get getPreviewInfoFont() {
-		return { fontSize: `${this.getCommonAdaptiveFontSize('previewInfo')}px` };
-	}
-	get getUseContactsTitleFont() {
-		return {
-			fontSize: `${this.getCommonAdaptiveFontSize('useContactsTitle')}px`,
-		};
-	}
-	get getUseContactsActionFont() {
-		return {
-			fontSize: `${this.getCommonAdaptiveFontSize('useContactsAction')}px`,
-		};
-	}
+  get getSlideTitleFont() {
+    return {
+      fontSize: `${this.getCustomAdaptiveSize({
+        xs: 12,
+        sm: 20,
+        md: 20,
+        lg: 30,
+      })}px`,
+    };
+  }
+  get getSlideContentFont() {
+    return {
+      fontSize: `${this.getCustomAdaptiveSize({
+        xs: 15,
+        sm: 20,
+        md: 20,
+        lg: 30,
+      })}px`,
+    };
+  }
+  get getPreviewTitleFont() {
+    return { fontSize: `${this.getCommonAdaptiveFontSize('previewTitle')}px` };
+  }
+  get getPreviewSubTitleFont() {
+    return {
+      fontSize: `${this.getCommonAdaptiveFontSize('previewSubtitle')}px`,
+    };
+  }
+  get getPreviewInfoFont() {
+    return { fontSize: `${this.getCommonAdaptiveFontSize('previewInfo')}px` };
+  }
+  get getUseContactsTitleFont() {
+    return {
+      fontSize: `${this.getCommonAdaptiveFontSize('useContactsTitle')}px`,
+    };
+  }
+  get getUseContactsActionFont() {
+    return {
+      fontSize: `${this.getCommonAdaptiveFontSize('useContactsAction')}px`,
+    };
+  }
 
-	@Watch('curStage')
-	onChangeCurStage(value: number) {
-		if (value === this.about[this.$i18n.locale].slides.length + 1) {
-			this.changeContactBar(true);
-		}
-	}
+  @Watch('curStage')
+  onChangeCurStage(value: number) {
+    if (value === this.about[this.$i18n.locale].slides.length + 1) {
+      this.changeContactBar(true);
+    }
+  }
 
-	@Watch('isShowContactBar')
-	onChangeContactBar(curValue: boolean, prevValue: boolean) {
-		// when we close contact bar - show prev stage
-		if (prevValue === true) {
-			this.curStage = this.curStage - 1;
-		} else {
-			this.curStage = this.about[this.$i18n.locale].slides.length + 1;
-		}
-	}
+  @Watch('isShowContactBar')
+  onChangeContactBar(curValue: boolean, prevValue: boolean) {
+    // when we close contact bar - show prev stage
+    if (prevValue === true) {
+      this.curStage = this.curStage - 1;
+    } else {
+      this.curStage = this.about[this.$i18n.locale].slides.length + 1;
+    }
+  }
 
-	created() {
-		this.changePageId(1);
-	}
-	beforeDestroy() {
-		this.changeContactBar(false);
-	}
+  created() {
+    this.changePageId(1);
+  }
+  beforeDestroy() {
+    this.changeContactBar(false);
+  }
 }
 </script>
 
