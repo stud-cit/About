c997121fc72fdb6fcf2f17e62510d70e70adb1ba
Konrem <lord.kazakovv@gmail.com>
2019-10-15 16:20:28 +0300

    fix( preview-page ): add margin to title in mobile version

--- client/components/preview-page.vue
+++ client/components/preview-page.vue
@@ -20,13 +20,13 @@
 			>
 				<div class="d-block">
 					<p
-						class="font-weight-bold"
+						class="font-weight-bold px-2"
 						:class="{ 'text-uppercase': isAboutPage }"
 						:style="getPreviewTitleFont"
 					>
 						{{ title }}
 					</p>
-					<p class="font-weight-bold" :style="getPreviewTitleFont">
+					<p class="font-weight-bold px-2" :style="getPreviewTitleFont">
 						{{ subtitle }}
 					</p>
 				</div>
