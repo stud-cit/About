67ed559cb163bbfa38975b97fa9b01694bf5df66
HELSIS <seagull.bogdan@gmail.com>
2019-10-18 23:52:39 +0300

    chore( config module ): update module flow

--- server/src/config/config.service.ts
+++ server/src/config/config.service.ts
@@ -6,32 +6,30 @@ import { EnvConfig } from './interfaces/envConfig.interface';
 
 @Injectable()
 export class ConfigService {
-  private readonly filePath: string;
-  private readonly envConfig: EnvConfig;
+	private readonly envConfig: EnvConfig;
 
-  constructor() {
-    this.filePath = process.env.NODE_ENV || '';
-    this.envConfig = {
-      ...process.env,
-      ...parse(readFileSync(`${this.filePath}.env`)),
-    };
-  }
+	constructor() {
+		this.envConfig = {
+			...process.env,
+			...parse(readFileSync(`.env.${process.env.NODE_ENV}`)),
+		};
+	}
 
-  getSetting(key: string): any {
-    const variable = this.envConfig[key];
+	get(key: string): any {
+		const variable = this.envConfig[key];
 
-    if (variable === undefined) {
-      return undefined;
-    }
+		if (this.envConfig[key] === undefined) {
+			throw TypeError(`The ${key} cannot be undefined`);
+		}
 
-    if (/true|false/.test(variable)) {
-      return Boolean(variable);
-    }
+		if (/true|false/.test(variable)) {
+			return Boolean(variable);
+		}
 
-    if (!Number.isNaN(+variable)) {
-      return +variable;
-    }
+		if (!Number.isNaN(+variable)) {
+			return +variable;
+		}
 
-    return this.envConfig[key];
-  }
+		return this.envConfig[key];
+	}
 }
