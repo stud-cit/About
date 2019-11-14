63ab83e4c9b3c070b7edd2c873ae5c2ea2967b97
v-bondarenko364 <vadim.b@develops.today>
2019-11-15 00:13:46 +0200

    feat( offers ): implement intersection animation representation blocks

--- client/pages/offers.vue
+++ client/pages/offers.vue
@@ -1,5 +1,5 @@
 <template>
-	<div>
+	<div ref="container">
 		<v-row justify="center">
 			<v-col cols="12" sm="10">
 				<PreviewPage
@@ -20,48 +20,58 @@
 					:justify="offer.contentPosition"
 					align="start"
 					:class="{ 'representation-xs': isXsOnly }"
+					ref="representations"
 				>
-					<v-col
-						cols="12"
-						xs="8"
-						md="8"
-						lg="8"
-						class="mb-12 mb-md-0 px-0"
-						:class="{ 'representation-image': isMdAndDown }"
+					<RepresentationBlock
+						:direction="offer.contentPosition"
+						:pose="representationToAnimate.includes(index) ? 'visible': 'hidden'"
+						class="representation-block"
+						:class="offer.contentPosition === 'start' ? 'image-left' : 'image-right'"
+						appear
 					>
-						<v-img
-							class="pb-12 pb-md-0"
-							:src="getDynamicAssets(offer.img)"
-							:lazy-src="getDynamicAssets('')"
-							:aspect-ratio="3 / 2"
-						/>
-					</v-col>
+						<v-col
+							cols="12"
+							xs="8"
+							md="8"
+							lg="8"
+							class="mb-12 mb-md-0 px-0"
+							:class="{ 'representation-image': isMdAndDown }"
+						>
+							<v-img
+								class="pb-12 pb-md-0"
+								:src="getDynamicAssets(offer.img)"
+								:aspect-ratio="3 / 2"
+							/>
+						</v-col>
 
-					<v-col
-						cols="12"
-						md="5"
-						class="representation-card px-0"
-						:class="
-							offer.contentPosition === 'start'
-								? 'right-bottom-position'
-								: 'left-bottom-position'
-						"
-					>
-						<v-card class="pa-4 px-lg-8 py-lg-12">
-							<v-card-title
-								class="bold-preview card-title mb-3"
-								:style="getCardTitleFont"
-								:class="isXsOnly ? 'card-title-mobile' : 'mb-3'"
-								>{{ offer.cardTitle }}</v-card-title
-							>
-							<v-card-text
-								class="font-weight-medium card-content"
-								:style="getCardContentFont"
-							>
-								<span>{{ offer.cardContent }}</span>
-							</v-card-text>
-						</v-card>
-					</v-col>
+						<v-col
+							cols="12"
+							md="5"
+							class="representation-card px-0"
+							:class="
+								offer.contentPosition === 'start'
+									? 'right-bottom-position'
+									: 'left-bottom-position'
+							"
+						>
+							<RepresentationContent>
+								<v-card class="pa-4 px-lg-8 py-lg-12">
+									<v-card-title
+										class="bold-preview card-title mb-3"
+										:style="getCardTitleFont"
+										:class="isXsOnly ? 'card-title-mobile' : 'mb-3'"
+										>{{ offer.cardTitle }}</v-card-title
+									>
+									<v-card-text
+										class="font-weight-medium card-content"
+										:style="getCardContentFont"
+									>
+										<span>{{ offer.cardContent }}</span>
+									</v-card-text>
+								</v-card>
+							</RepresentationContent>
+						</v-col>
+					</RepresentationBlock>
 				</v-row>
 			</v-col>
 			<product-footer class="max-width" />
@@ -72,6 +82,7 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { Getter, Mutation } from 'vuex-class';
+import posed from 'vue-pose';
 
 import PruductFooter from '@/components/product-footer.vue';
 import PreviewPage from '@/components/preview-page.vue';
@@ -87,11 +98,39 @@ import ScrollBar from '@/components/scroll-bar.vue';
 		'product-footer': PruductFooter,
 		PreviewPage,
 		ScrollBar,
+		RepresentationBlock: posed.div({
+			visible: {
+				opacity: 1,
+				x: 0,
+				transition: { duration: 200 },
+				beforeChildren: true,
+			},
+			hidden: {
+				opacity: 0,
+				x: ({direction}) => direction === 'start' ? -40 : 40,
+			},
+		}),
+		RepresentationContent: posed.div({
+			visible: {
+				opacity: 1,
+				y: 0,
+				delay: 75,
+			},
+			hidden: {
+				opacity: 0,
+				y: 20,
+			},
+		}),
 	},
 })
 export default class OffersPage extends Vue {
 	@Getter('OffersModule/getStage') weOffers;
 	@Mutation('changePageId') changePageId;
+
+	observers: IntersectionObserver[] = [];
+
+	// current index for start animation during scroll
+	representationToAnimate: number[] = [];
 
 	get isXsOnly() {
 		return this.$breakpoint ? this.$breakpoint.is.xsOnly : false;
@@ -111,6 +150,39 @@ export default class OffersPage extends Vue {
 	created() {
 		this.changePageId(2);
 	}
+
+
+// TODO: переделай картинку, чтобы она была v-img и грузилась сразу
+	setAnimation(entry, representationIndex, observer) {
+		if (
+			entry.intersectionRatio > 0 &&
+			!this.representationToAnimate.includes(representationIndex)
+		) {
+			this.representationToAnimate.push(representationIndex);
+		}
+
+
+		if(this.representationToAnimate.includes(representationIndex)) {
+			observer.disconnect();
+		}
+	}
+
+	mounted() {
+		const representations: any = this.$refs.representations;
+
+		this.observers = representations.map((representation, index) => {
+			const options = { threshold: 0.65 };
+			const observer = new IntersectionObserver(([entry], observer) => this.setAnimation(entry, index, observer), options);
+			observer.observe(representation);
+			return observer;
+		});
+	}
+
+	beforeDestroy() {
+    this.observers.forEach( observer => {
+			observer.disconnect();
+		});
+  }
 }
 </script>
 
@@ -129,6 +201,10 @@ export default class OffersPage extends Vue {
 	position: absolute
 	overflow: hidden
 
+.representation-block
+	width: 100vw
+	display: flex
+
 .representation-xs
 	padding-bottom: 15vh
 
@@ -140,6 +216,12 @@ export default class OffersPage extends Vue {
 
 .card-title-mobile
 	line-height: 1
+
+.image-left
+	justify-content: flex-start
+
+.image-right
+	justify-content: flex-end
 
 .right-bottom-position
 	right: 0
