6b217d7fa297a8b239ec17f48f78066aeb1ee6a0
HELSIS <seagull.bogdan@gmail.com>
2019-11-21 02:22:43 +0200

    doc( server/user decorator ): add ts-doc description

--- server/src/common/decorators/user.decorator.ts
+++ server/src/common/decorators/user.decorator.ts
@@ -2,6 +2,11 @@ import { createParamDecorator } from '@nestjs/common';
 
 import { UserEntity } from '../../modules/user/user.entity';
 
+/**
+ * [createParamDecorator description]
+ * @param  (data [description]
+ * @return       [description]
+ */
 export const User = createParamDecorator(
 	(data: string, req): UserEntity => {
 		return data ? req.user && req.user[data] : req.user;
