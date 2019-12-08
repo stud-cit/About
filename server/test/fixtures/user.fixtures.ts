619ee9110d8bd97e3bb2f4f6eeef75ac26b2b658
HELSIS <seagull.bogdan@gmail.com>
2019-12-09 00:38:12 +0200

    test( server/pages ): add request fixture

--- server/test/fixtures/user.fixtures.ts
+++ server/test/fixtures/user.fixtures.ts
@@ -1,6 +1,8 @@
 import { internet } from 'faker';
 
-export class User {
+import { UserRequest } from '../../src/modules/user/dto/user.dto';
+
+export class User implements UserRequest {
 	constructor(
 		readonly email: string = internet.email(),
 		readonly password: string = internet.password(),
