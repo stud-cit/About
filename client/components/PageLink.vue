458a0f5be0672650ea95d04aa5c8d6a33a770abe
HELSIS <seagull.bogdan@gmail.com>
2019-12-11 04:57:36 +0200

    feat( client/layout ): add dashboard layout

---
+++ client/components/PageLink.vue
@@ -0,0 +1,14 @@
+<template>
+	<v-btn v-bind="{ ...$attrs, ...page.attr }" v-on="$listeners" class="mx-3">
+		<v-icon v-html="page.icon" />
+	</v-btn>
+</template>
+
+<script lang="ts">
+	import { Component, Vue, Prop } from 'vue-property-decorator';
+
+	@Component
+	export default class PageLink extends Vue {
+		@Prop({ type: Object, required: true }) page: object;
+	}
+</script>
