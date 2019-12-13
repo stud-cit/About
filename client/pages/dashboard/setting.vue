397d1794b917a1caa50322299715bb595cc37320
HELSIS <seagull.bogdan@gmail.com>
2019-12-13 23:29:04 +0200

    feat( client/dashboard pages  ): add base view for setting page

---
+++ client/pages/dashboard/setting.vue
@@ -0,0 +1,33 @@
+<template>
+	<v-row>
+		<v-col xs="12">
+			<v-card color="grey darken-4" tile flat>
+				<contact-bar default :style="{ position: 'relative' }" />
+			</v-card>
+		</v-col>
+	</v-row>
+</template>
+
+<script lang="ts">
+	import { Component, Vue } from 'vue-property-decorator';
+	import { Getter, Mutation } from 'vuex-class';
+
+	@Component({
+		layout: 'dashboard',
+		head() {
+			return {
+				title: 'Setting',
+			};
+		},
+		components: {
+			ContactBar: () => import('~/components/contact-bar'),
+		},
+	})
+	export default class DashboardSettingPage extends Vue {
+		@Mutation('changeContactBar') changeContactBar;
+
+		mounted() {
+			this.changeContactBar(true);
+		}
+	}
+</script>
